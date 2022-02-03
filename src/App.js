17dfcc8f265e3ffaaaf361f7b86291de83640b93
Florid11 <florid.arifaj@hotmail.com>
2022-02-03 13:43:41 +0100

    Map Function & Demo Data added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+const comp =[
+  {
+  image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    title: "The TLD",
+    description: "Does the domain extension math the language of the domain name?"
+  },
+  {
+    image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    title: "The TLD",
+    description: "Does the domain extension math the language of the domain name?"
+  },
+  {
+    image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    title: "The TLD",
+    description: "Does the domain extension math the language of the domain name?"
+  },
+  {
+    image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    title: "The TLD",
+    description: "Does the domain extension math the language of the domain name?"
+  },
+  {
+    image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    title: "The TLD",
+    description: "Does the domain extension math the language of the domain name?"
+  },
+  {
+    image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    title: "The TLD",
+    description: "Does the domain extension math the language of the domain name?"
+  }
+]
   return (
-    <div className="App">
-
+    <div className="app">
+      {comp.map((components) => {
+            return(
+              <div className='box'>
+                <img src={components.image} alt="Logo"/>
+                <p>{` |   Title: ${components.title}  |  Decription: ${components.description}`}</p>
+              </div>
+            );
+      })}
     </div>
   );
 }
