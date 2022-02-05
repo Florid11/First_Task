b9a59ff0db336581dec4927d7e565d962bea0231
Florid11 <florid.arifaj@hotmail.com>
2022-02-05 07:46:19 +0100

    Changes made in components css

--- src/components/list.js
+++ src/components/list.js
@@ -1,7 +1,25 @@
 import React from "react";
+import classes from "./list.module.css";
 
-const List = () => {
-  
+const List = (props) => {
+  return(
+      <div className={classes.layout}>
+          <div>
+              <h3 className={classes.header}>{props.date}</h3>
+              <div>
+                  <p className={classes.btn}>{props.job}</p>
+                  <p className={classes.img}>{props.image}</p>
+                  <h3 className={classes.header}>{props.name}</h3>
+              </div>
+                <div>
+                    {props.decription}
+                </div>
+              <div>
+                  <button className={classes.btn_down} onClick='/'> Download</button>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default List;
